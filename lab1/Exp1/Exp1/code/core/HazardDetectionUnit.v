b616939ce67b9d00386066b0c3e088256b5d92b2
l1ght <2504082957@qq.com>
2021-10-08 11:15:21 +0800

    trial fill

--- lab1/Exp1/Exp1/code/core/HazardDetectionUnit.v
+++ lab1/Exp1/Exp1/code/core/HazardDetectionUnit.v
@@ -20,11 +20,14 @@ module HazardDetectionUnit(
         forward_ctrl_B = 2'b00;
         forward_ctrl_ls = 1'b0;
         PC_EN_IF = 1;
+
         reg_FD_EN = 1;
         reg_DE_EN = 1;
         reg_EM_EN = 1;
         reg_MW_EN = 1;
+
         reg_FD_stall = 0;
+
         reg_FD_flush = 0;
         reg_DE_flush = 0;
         reg_EM_flush = 0;
@@ -33,8 +36,6 @@ module HazardDetectionUnit(
     // 10: Load-use type with data hazard
     // 11: Branch type with control hazard
 
-
-        
         // Forward from control signal 3 if this is a load-use hazard
 
 
@@ -46,7 +47,8 @@ module HazardDetectionUnit(
         else if (hazard_optype_ID[1] && hazard_optype_ID[0]) begin
             // 11: branch type
             // predict the branch
-
+            reg_FD_EN = 1'b0;
+            reg_FD_flush = 1'b1; // the pipeline stalls
         end
 
         else if (!hazard_optype_ID[1] && hazard_optype_ID[0]) begin
@@ -55,23 +57,33 @@ module HazardDetectionUnit(
             if (rs1use_ID && rs1_ID != 0 && rs1_ID == rd_EXE) begin
                 // forward A from EX
                 forward_ctrl_A = 2'b01;
+                PC_EN_IF = 0;
+                reg_FD_stall = 1;
+
+
             end
 
             if (rs1use_ID && rs1_ID != 0 && rs1_ID == rd_MEM) begin
                 // forward A from MEM
                 forward_ctrl_A = 2'b10;
+                PC_EN_IF = 0;
+                reg_FD_stall = 1;
 
             end
 
             if (rs2use_ID && rs2_ID != 0 && rs2_ID == rd_EXE) begin
                 // forward B from EX
                 forward_ctrl_B = 2'b01;
+                PC_EN_IF = 0;
+                reg_FD_stall = 1;
 
             end
             
             if (rs2use_ID && rs2_ID != 0 && rs2_ID == rd_MEM) begin
                 // forward B from MEM
                 forward_ctrl_B = 2'b10;
+                PC_EN_IF = 0;
+                reg_FD_stall = 1;
             end
 
         end
