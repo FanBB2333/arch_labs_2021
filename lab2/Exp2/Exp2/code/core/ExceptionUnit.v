3fa4196139261774756109d2fe4e876c4908cc95
l1ght <2504082957@qq.com>
2021-10-12 13:12:18 +0800

    Add basic logic of exception

--- lab2/Exp2/Exp2/code/core/ExceptionUnit.v
+++ lab2/Exp2/Exp2/code/core/ExceptionUnit.v
@@ -2,7 +2,7 @@
 
 module ExceptionUnit(
     input clk, rst,
-    input csr_rw_in, // if the inst is CSR inst
+    input csr_rw_in, // if the inst is CSR inst, CSR_valid = CSRRW | CSRRS | CSRRC | CSRRWI | CSRRSI | CSRRCI
     input[1:0] csr_wsc_mode_in, // inst_MEM[13:12]
     input csr_w_imm_mux, // CSRRWI | CSRRSI | CSRRCI
     input[11:0] csr_rw_addr_in, // inst_MEM[31:20], represents the csr reg
@@ -41,5 +41,30 @@ module ExceptionUnit(
         .rdata(csr_r_data_out),.mstatus(mstatus));
 
     //According to the diagram, design the Exception Unit
+    always @(posedge clk) begin
+        if(rst) begin
+            csr_raddr <= 0;
+            csr_waddr <= 0;
+            csr_wdata <= 0;
+            csr_wsc <= 0;
+            csr_w <= 0;
+        end
+        else begin
+            csr_raddr <= csr_rw_addr_in;
+            csr_waddr <= csr_rw_addr_in;
+            csr_wdata <= csr_w_data_reg;
+            csr_wsc <= csr_wsc_mode_in;
+
+            csr_w <= csr_rw_in;
+            if (mret)begin
+                
+            end
+        end
+        
+
+
+        
+    end
+
 
 endmodule
