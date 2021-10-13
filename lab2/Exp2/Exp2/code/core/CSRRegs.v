332842ca89de84b426aa23f8d2bcc973bff38aec
l1ght <2504082957@qq.com>
2021-10-13 19:41:53 +0800

    Add the CSR names

--- lab2/Exp2/Exp2/code/core/CSRRegs.v
+++ lab2/Exp2/Exp2/code/core/CSRRegs.v
@@ -13,7 +13,7 @@ module CSRRegs(
     reg[31:0] CSR [0:15];
 
     // Address mapping. The address is 12 bits, but only 4 bits are used in this module.
-    wire raddr_valid = raddr[11:7] == 5'h6 && raddr[5:3] == 3'h0;
+    wire raddr_valid = raddr[11:7] == 5'h6 && raddr[5:3] == 3'h0; // [11:7] == 2'b00110
     wire[3:0] raddr_map = (raddr[6] << 3) + raddr[2:0];
     wire waddr_valid = waddr[11:7] == 5'h6 && waddr[5:3] == 3'h0;
     wire[3:0] waddr_map = (waddr[6] << 3) + waddr[2:0];
@@ -24,22 +24,24 @@ module CSRRegs(
 
     always@(posedge clk or posedge rst) begin
         if(rst) begin
-			CSR[0] <= 32'h88;
-			CSR[1] <= 0;
-			CSR[2] <= 0;
-			CSR[3] <= 0;
-			CSR[4] <= 32'hfff;
-			CSR[5] <= 0;
-			CSR[6] <= 0;
-			CSR[7] <= 0;
-			CSR[8] <= 0;
-			CSR[9] <= 0;
-			CSR[10] <= 0;
-			CSR[11] <= 0;
-			CSR[12] <= 0;
-			CSR[13] <= 0;
-			CSR[14] <= 0;
-			CSR[15] <= 0;
+            // addr[6] == 0
+			CSR[0] <= 32'h88; // mstatus
+			CSR[1] <= 0; // misa
+			CSR[2] <= 0; // medeleg
+			CSR[3] <= 0; // mideleg
+			CSR[4] <= 32'hfff; // mie
+			CSR[5] <= 0; // mtvec
+			CSR[6] <= 0; // mcounteren
+			CSR[7] <= 0; // 
+            // addr[6] == 1
+			CSR[8] <= 0; // mscratch
+			CSR[9] <= 0; // mepc
+			CSR[10] <= 0; // mcause
+			CSR[11] <= 0; // mtval
+			CSR[12] <= 0; // mip
+			CSR[13] <= 0; // 
+			CSR[14] <= 0; // 
+			CSR[15] <= 0; // 
 		end
         else if(csr_w) begin
             case(csr_wsc_mode)
