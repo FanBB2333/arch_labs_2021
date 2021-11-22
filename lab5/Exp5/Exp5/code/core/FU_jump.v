28fba4b6841e32886a162868f67e097c3d432bc4
l1ght <2504082957@qq.com>
2021-11-22 12:22:12 +0800

    add jump module

--- lab5/Exp5/Exp5/code/core/FU_jump.v
+++ lab5/Exp5/Exp5/code/core/FU_jump.v
@@ -18,6 +18,23 @@ module FU_jump(
 	reg[2:0] cmp_ctrl_reg;
 	reg[31:0] rs1_data_reg, rs2_data_reg, imm_reg, PC_reg;
 
-	...            //to fill sth.in
+	//to fill sth.in
+	always@(posedge clk) begin
+		if(EN & ~state) begin // state == 0
+			JALR_reg <= JALR;
+			cmp_ctrl_reg <= cmp_ctrl;
+			rs1_data_reg <= rs1_data;
+			rs2_data_reg <= rs2_data;
+			imm_reg <= imm;
+			PC_reg <= PC;
+			state <= 1'b1; // state is ready
+		end
+		else state <= 0;
+	end
+	
+	cmp_32 cmp(.a(rs1_data_reg), .b(rs2_data_reg), .ctrl(cmp_ctrl_reg), .c(cmp_res));
+	add_32 a(.a(), .b(imm_reg), .c(PC_jump));
+	add_32 b(.a(PC_reg), .b(32'b4), .c(PC_wb));	
+
 
 endmodule
