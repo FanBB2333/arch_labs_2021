a5c2f4010caa0274121d5c1426ecbc969d93fc8d
L1ght <2504082957@qq.com>
2021-12-18 18:53:24 +0800

    oops

--- lab5/Exp5/Exp5/code/sim/core_sim.v
+++ lab5/Exp5/Exp5/code/sim/core_sim.v
@@ -13,11 +13,17 @@ module core_sim;
         .interrupter(1'b0)
     );
 
-    initial begin
-        clk = 0;
-        rst = 1;
-        #2 rst = 0;
-    end
-    always #1 clk = ~clk;
+	initial begin
+		// Initialize Inputs
+		clk = 0;
+		rstn = 0;
+
+		// Wait 100 ns for global reset to finish
+		#95 rstn = 1;
+        
+		// Add stimulus here
+	end
+	
+	initial forever #10 clk = ~clk;
 
 endmodule
